c338aa3eefa16e06f59198fc2a8152ae63006068
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-03 22:17:26 +0000

    WIP

--- config.py
+++ config.py
@@ -7,8 +7,15 @@ config['settings'] = {
 }
 
 # Add config settings for Cassandra
+config['cassandra'] = {
+    'host': 'http://127.0.0.1',
+    'port': 7000
+}
 
 # Add config settings for Kafka
+config['kafka'] = {
+    'host': 'http://127.0.0.1'
+}
 
 with open('./config.ini', 'w') as f:
     config.write(f)
