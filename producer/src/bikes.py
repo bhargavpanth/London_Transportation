015dc7e671d3383e857afa8c5e610dc9cfba0243
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-04 22:57:31 +0000

    WIP - adding bikepoint specifics

--- producer/src/bikes.py
+++ producer/src/bikes.py
@@ -1,3 +1,12 @@
 class Bike:
     def __init__(self):
         self.end_point = 'https://api.tfl.gov.uk/BikePoint'
+
+    def fetch_all(self):
+        # GET req on self.end_point
+        pass
+
+    def get_data_by_bike_point_id(self, id: str):
+        # GET req on f'{self.endpoint}/{id}'
+        # where id is f'Bikepoints_{num}' and 1 <= num <= 820
+        pass
