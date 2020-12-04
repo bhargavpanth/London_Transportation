5c66920ed300cb4f761d6e3fca3625e997c467c0
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-04 23:50:31 +0000

    WIP - add cron tab

---
+++ producer/main.py
@@ -0,0 +1,10 @@
+import argparse
+from src.bikes import Bike
+
+def main(mode: str, cron_tab: str):
+    pass
+
+if __name__ == '__main__':
+    mode = ''
+    cron_tab = ''
+    main(mode, cron_tab)
