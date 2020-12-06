318bfdbc46acd1e05fc6f89f69cfc33a2c15082c
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-06 21:24:21 +0000

    WIP

--- producer/src/bus.py
+++ producer/src/bus.py
@@ -1,3 +1,10 @@
+'''
+disruption end-point
+https://api.tfl.gov.uk/Line/Mode/{modes}/Disruption
+where mode are ['bus', 'cable-car', 'coach', 'cycle', 'dlr', 'tube', 'walking', 'tube', 'tram', 'tflrail', 'taxi', 'river-tour', 'river-bus', 'replacement-bus', 'overground', 'national-rail', 'interchange-secure', 'interchange-keep-sitting']
+fetch modes - https://api.tfl.gov.uk/Line/Meta/Modes
+'''
+
 class Bus:
     def __init__(self):
         self.end_point = 'https://api.tfl.gov.uk/Line/Route'
