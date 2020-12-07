fedf8b2e1ce11e5fcf3fc3e19980f690a6cbb572
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-07 21:16:32 +0000

    WIP

---
+++ producer/src/utils.py
@@ -0,0 +1,7 @@
+'''
+where mode are ['bus', 'cable-car', 'coach', 'cycle', 'dlr', 'tube', 'walking', 'tube', 'tram', 'tflrail', 'taxi', 'river-tour', 'river-bus', 'replacement-bus', 'overground', 'national-rail', 'interchange-secure', 'interchange-keep-sitting']
+fetch modes - https://api.tfl.gov.uk/Line/Meta/Modes
+'''
+
+def get_transportation_modes():
+    pass
