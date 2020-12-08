e1753de10d7d0d94d875d07faf4d75490e590235
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-08 23:39:45 +0000

    WIP

--- producer/src/utils.py
+++ producer/src/utils.py
@@ -3,8 +3,13 @@ where mode are ['bus', 'cable-car', 'coach', 'cycle', 'dlr', 'tube', 'walking', 
 fetch modes - https://api.tfl.gov.uk/Line/Meta/Modes
 '''
 import urllib
+import requests
+import json
 
 def get_transportation_modes():
     url = 'https://api.tfl.gov.uk/Line/Meta/Modes'
-    req = urllib.request.Request(url)
-    pass
+    res = requests.get(url)
+    if res.status_code == 200:
+        return json.loads(res.content.decode('utf-8'))
+    else:
+        return None
