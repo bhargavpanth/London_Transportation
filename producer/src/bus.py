006eb30552941bde92d5e52a138a0a2d0ce5acac
Bhargav Panth <bhargavrpanth@gmail.com>
2020-12-09 22:29:10 +0000

    WIP

--- producer/src/bus.py
+++ producer/src/bus.py
@@ -2,10 +2,14 @@
 disruption end-point
 https://api.tfl.gov.uk/Line/Mode/{modes}/Disruption
 '''
+from utils import get_transportation_modes
 
 class Bus:
     def __init__(self):
         self.end_point = 'https://api.tfl.gov.uk/Line/Route'
+
+    def fetch_transportation_mode(self):
+        return get_transportation_modes()
 
     def fetch_all(self):
         pass
